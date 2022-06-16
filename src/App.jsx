e595496fa1628046dcdcb71b3cdb7958c224ad1e
Billy Arredondo <billy.arredondo@pucp.pe>
2022-06-16 18:53:08 -0500

    Adding TaskBanner, Container components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import './App.css';
+import { Container } from './components/Container';
+import { TaskBanner } from './components/TaskBanner';
 
 function App() {
-  return (
-    <>
-    </>
-  );
+
+    const [userName, setUserName] = useState('Billy');
+
+    return (
+        <>
+            <TaskBanner userName={userName} />
+            <Container />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/TaskBanner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const TaskBanner = ({ userName, taskItems}) => {
+    return (
+        <>
+            <h2>{ userName }'s tasks</h2>
+        </>
+    )
+}
